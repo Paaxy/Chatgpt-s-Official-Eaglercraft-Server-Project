053e3619f90f288a7987008d0afebec43084532a
Paaxy <163489393+Paaxy@users.noreply.github.com>
2024-08-30 16:59:17 +0900

    Create MyFirstPlugin.java

---
+++ MyFirstPlugin.java
@@ -0,0 +1,18 @@
+package com.example.myfirstplugin;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MyFirstPlugin extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.setJoinMessage("Welcome to the server, " + event.getPlayer().getName() + "!");
+    }
+}
